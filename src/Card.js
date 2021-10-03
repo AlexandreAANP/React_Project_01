d673247e336b71a996c411bf1dd8f11a1da040ea
AlexandreAANP <alexandreaanp@gmail.com>
2021-10-03 16:13:50 +0100

    00

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+import Cart from './carts/2_Ouro.svg';
+
+function Card (){
+	return(
+		<div>
+			<img src={Cart}/>
+		</div>
+		);
+}
+export default Card;
---
+++ src/carts/teste_component.js
@@ -0,0 +1,5 @@
+function comp () {
+	return(
+		<h2>Comp</h2>
+		);
+}
